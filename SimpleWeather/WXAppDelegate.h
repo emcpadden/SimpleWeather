0a55513de4beaf0af5c6d2960a6f3da31bef8ec2
Ed McPadden <emcpadden@gmail.com>
2014-10-31 14:36:47 -0400

    Initial Commit

---
+++ SimpleWeather/WXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WXAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Ed McPadden on 10/31/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
